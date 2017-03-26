1cdee808de2b8085bda3c57ce4533ca9518823ae
simpsonm <themattsimpson@gmail.com>
2017-03-26 16:09:31 -0500

    began working real data example

--- stanspatial.R
+++ stanspatial.R
@@ -1,10 +1,46 @@
 library(rstan)
+library(tigris)
+library(dplyr) ## for easy data frame manipulations
+set.seed(34345398) ## for reproducing my simulations
+
+coincfull <- read.csv("data/county/income/ACS_15_5YR_S1902_with_ann.csv", skip = 1)
+coinc <- select(coincfull, Id, Id2, Geography,
+                Mean.income..dollars...Estimate..All.households)
+colnames(coinc) <- c("id", "id.co", "name", "income.mean")
+
+coedufull <- read.csv("data/county/edu/ACS_15_5YR_S1501_with_ann.csv", skip = 1)
+coedu <- coedufull %>%
+  select(Id, Id2, Geography, contains("Percent..Estimate..Population.25.years.and.over")) %>%
+  select(-Percent..Estimate..Population.25.years.and.over)
+
+colnames(coedu) <- c("id", "id.co", "name",
+                     paste("edu.over25.",
+                           c("lessHS", "someHS", "HS", "someCol", "assoc", "bach", "gradprof")))
+
+coracefull <- read.csv("data/county/race/ACS_15_5YR_DP05_with_ann.csv", skip = 1)
+corace <- coracefull %>%
+  select(id = Id, id.co = Id2, name = Geography,
+         race.white = Percent..RACE...One.race...White,
+         race.black = Percent..RACE...One.race...Black.or.African.American,
+         race.natamer = Percent..RACE...One.race...American.Indian.and.Alaska.Native,
+         race.asian = Percent..RACE...One.race...Asian,
+         race.islander = Percent..RACE...One.race...Native.Hawaiian.and.Other.Pacific.Islander,
+         race.other = Percent..RACE...One.race...Some.other.race,
+         race.twoplus = Percent..RACE...Two.or.more.races,
+         hispanic = Percent..HISPANIC.OR.LATINO.AND.RACE...Total.population...Hispanic.or.Latino..of.any.race.) %>% mutate(race.other = race.other  + race.islander) %>% select(-race.islander)
+
+Sys.setlocale('LC_ALL','C') ## to deal with a string with weird characters in it
+codata <- full_join(coinc, full_join(coedu, corace)) %>% 
+  mutate(state = sapply(name, function(x){strsplit(as.character(x), ", ")[[1]][2]}))
+
 
 #####  fit a normal distribution
-y <- rnorm(1000, 4.0, 1.2)  # simulate fake data
+y <- rnorm(100, 4.0, 1.2)  # simulate fake data
 
 ## create a list of each variable defined in the data block
-normaldat <- list(n_obs = length(y), y = y, mu_mn = 0, mu_sd = 10)
+normaldat <- list(n_obs = length(y), y = y,
+                  mu_prior_mn = 0, mu_prior_sd = 10,
+                  sig_prior_scale = 10, sig_prior_df = 5)
 
 ## create the log posterior and make sure the sampler initializes
 normalfit0 <- stan("normal.stan", data = normaldat, chains = 1, iter = 1)
@@ -22,13 +58,13 @@ traceplot(normalfit)
 summary(normalfit)$summary ## combines all chains together
 
 ##### fit a hierarchical model
-J <- 3 # number of groups
-N <- 100 # sample size per group
+ngroup <- 3 # number of groups
+nobs <- 25 # sample size per group
 mus <- c(1.2, 1.4, 1.6)
 sigma <- 2.5
-y <- t(sapply(mus, function(x) rnorm(N, x, sigma))) ## need it to be J x N
+y <- t(sapply(mus, function(x) rnorm(N, x, sigma))) ## need it to be ngroup x nobs
 
-hierdat <- list(n_obs = N, n_group = J, y = (y - mean(y))/sd(y), mu_mn = 0, mu_sd = 10)
+hierdat <- list(n_obs = nobs, n_group = ngroup, y = y, mu_mn = 0, mu_sd = 10)
 
 hierfit0 <- stan("normalhier.stan", data = hierdat, chains = 1, iter = 1)
 
